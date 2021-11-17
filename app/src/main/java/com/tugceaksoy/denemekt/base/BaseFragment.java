bc4fda8079a69dd07a496f8c4a938a33271b9bf2
TUĞÇE AKSOY <tugceaksy60@gmail.com>
2021-11-17 15:04:58 +0300

    First

---
+++ app/src/main/java/com/tugceaksoy/denemekt/base/BaseFragment.java
@@ -0,0 +1,37 @@
+package com.tugceaksoy.denemekt.base;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tugceaksoy.denemekt.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BaseFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BaseFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_base, container, false);
+    }
+
+    public void replaceFragmentWithBackStack(int contId, Fragment fragment) {
+
+    }
+}
